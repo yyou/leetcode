cbc54b7fcabcbd59aa1e399667c3f19d8d41eef5
Yang You <yang_you@gpsynergy.com.au>
2021-01-11 22:07:24 +1100

    Added solution for question 332 - coin changes

---
+++ 332-coin-change.cs
@@ -0,0 +1,48 @@
+public class Solution332
+{
+    public int CoinChange(int[] coins, int amount)
+    {
+        var memo = new Dictionary<int, int>();
+        return CoinChangeImpl(coins, amount, memo);
+    }
+
+    private int CoinChangeImpl(Int32[] coins, int amount, Dictionary<int, int> memo)
+    {
+        if (amount == 0)
+        {
+            return 0;
+        }
+
+        if (amount < 0)
+        {
+            return -1;
+        }
+
+        if (memo.ContainsKey(amount))
+        {
+            return memo[amount];
+        }
+
+        var coinCount = Int32.MaxValue;
+
+        foreach (var coin in coins)
+        {
+            var subCoinCount = CoinChangeImpl(coins, amount - coin, memo);
+            if (subCoinCount == -1)
+            {
+                continue;
+            }
+
+            coinCount = Math.Min(coinCount, subCoinCount + 1);
+        }
+
+        if (coinCount == Int32.MaxValue)
+        {
+            memo[amount] = -1;
+            return -1;
+        }
+
+        memo[amount] = coinCount;
+        return coinCount;
+    }
+}
--- 509-fabonacci-number.cs
+++ 509-fabonacci-number.cs
@@ -1,16 +1,16 @@
 public class 509Solution {
     public int Fib(int n) {
-        if (n <= 0) {
+        if (n < 1) {
             return 0;
         }
         
-        if (n == 1) {
+        if (n == 1 || n == 2) {
             return 1;
         }
         
-        var n0 = 0;
+        var n0 = 1;
         var n1 = 1;
-        var idx = 2;        
+        var idx = 3;        
         while (idx <= n) {
             var tmp = n1 + n0;
             n0 = n1;
