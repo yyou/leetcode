a3138e40e7becaa6ebc9e755379834c1cc54ec95
Yang You <yang_you@gpsynergy.com.au>
2021-01-17 23:58:41 +1100

    Added solution for question 3 - find longest substring without repeating character

---
+++ 3-longest-substring-without-repeating-character.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution3
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        var window = new Dictionary<Char, Int32>();
+
+        int left = 0, right = 0;
+        int maxLength = Int32.MinValue;
+
+        while (right < s.Length)
+        {
+            char c = s[right];
+
+            if (!window.ContainsKey(c))
+            {
+                maxLength = Math.Max(maxLength, right - left + 1);
+                window[c] = right;
+            }
+            else
+            {
+                var previousLocation = window[c];
+                maxLength = Math.Max(maxLength, right - left);
+                while (left < previousLocation + 1)
+                {
+                    window.Remove(s[left]);
+                    left++;
+                }
+                window[c] = right;
+            }
+
+            right++;
+        }
+
+        return maxLength == Int32.MinValue ? 0 : maxLength;
+    }
+}
