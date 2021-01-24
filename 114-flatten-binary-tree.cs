f502b29a093fdf009a59483b57b4a41c7acbe94a
Yang You <yy_318@hotmail.com>
2021-01-24 23:13:42 +1100

    Added solution for question 114 - flatten binary tree

---
+++ 114-flatten-binary-tree.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace leetcode
+{
+    public class Solution114
+    {
+        public void Flatten(TreeNode root)
+        {
+            if (root == null) {
+                return;
+            }
+
+            Flatten(root.left);
+            Flatten(root.right);
+
+            if (root.left != null) {
+                var p = root.left;            
+                while (p.right != null) {
+                    p = p.right;
+                }
+
+                p.right = root.right;
+                root.right = root.left;
+                root.left = null;
+            }
+        }
+    }
+}
