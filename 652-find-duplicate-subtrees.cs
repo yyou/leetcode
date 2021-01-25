beffc902fa13439a23324434cef354e78bd015ea
Yang You <yy_318@hotmail.com>
2021-01-25 22:00:42 +1100

    Added solution for question 652 - find duplicate subtrees

---
+++ 652-find-duplicate-subtrees.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace leetcode
+{
+    public class Solution652
+    {
+        public IList<TreeNode> FindDuplicateSubtrees(TreeNode root)
+        {
+            var dict = new Dictionary<int, Boolean>();
+            var result = new List<TreeNode>();
+            FindDuplicateSubtrees(root, dict, result);
+            return result;
+        }
+
+        private int FindDuplicateSubtrees(
+            TreeNode node, Dictionary<int, Boolean> dict, List<TreeNode> result)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            var s = String.Format(
+                "{0} {1} {2}",
+                node.val,
+                FindDuplicateSubtrees(node.left, dict, result),
+                FindDuplicateSubtrees(node.right, dict, result));
+            var code = s.GetHashCode();
+            if (dict.ContainsKey(code))
+            {
+                if (dict[code] == false)
+                {
+                    result.Add(node);
+                    dict[code] = true;
+                }
+            }
+            else
+            {
+                dict[code] = false;
+            }
+
+            return code;
+        }
+    }
+}
---
+++ TreeNodeExtensions.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+
+namespace leetcode {
+    public static class TreeNodeExtensions {
+        public static String Serialize(this TreeNode node) {
+            if (node == null) {
+                return "#";
+            }
+            return String.Format(
+                "({0}, {1}, {2})", 
+                node.val, 
+                node.left.Serialize(),
+                node.right.Serialize());
+        }
+    }
+}
