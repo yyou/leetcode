1f8443212abe49788bfae323a488d963e68472d9
Yang You <yy_318@hotmail.com>
2021-02-15 19:30:33 +1100

    Add another solution for question 130 with DFS

--- 130-surrounded-regions.cs
+++ 130-surrounded-regions.cs
@@ -63,4 +63,74 @@ namespace leetcode {
             }
         }
     }
+
+    public class Solution130DFS {
+        public void Solve(char[][] board) {
+            if (board.Length == 0 || board[0].Length == 0) {
+                return;
+            }
+
+            var rowCount = board.Length;
+            var columnCount = board[0].Length;
+
+            for (var i = 0; i < rowCount; ++i) {
+                if (board[i][0] == 'O') {
+                    BoundaryDFS(board, i, 0);
+                }
+
+                if (board[i][columnCount - 1] == 'O') {
+                    BoundaryDFS(board, i, columnCount - 1);
+                }
+            }
+
+            for (var j = 0; j < columnCount; ++j) {
+                if (board[0][j] == 'O') {
+                    BoundaryDFS(board, 0, j);
+                }
+
+                if (board[rowCount - 1][j] == 'O') {
+                    BoundaryDFS(board, rowCount - 1, j);
+                }
+            }
+
+            for (var i = 1; i < rowCount - 1; ++i) {
+                for (var j = 1; j < columnCount - 1; ++j) {
+                    if (board[i][j] == 'O') {
+                        board[i][j] = 'X';
+                    }
+                }
+            }
+
+            for (var i = 0; i < rowCount; ++i) {
+                for (var j = 0; j < columnCount; ++j) {
+                    if (board[i][j] == 'Y') {
+                        board[i][j] = 'O';
+                    }
+                }
+            }
+        }
+
+        private void BoundaryDFS(char[][] board, int rowIndex, int columnIndex) {
+            board[rowIndex][columnIndex] = 'Y';
+
+            var rowCount = board.Length;
+            var columnCount = board[0].Length;
+
+            if (rowIndex + 1 < rowCount && board[rowIndex + 1][columnIndex] == 'O') {
+                BoundaryDFS(board, rowIndex + 1, columnIndex);
+            }
+
+            if (rowIndex - 1 >= 0 && board[rowIndex - 1][columnIndex] == 'O') {
+                BoundaryDFS(board, rowIndex - 1, columnIndex);
+            }
+
+            if (columnIndex + 1 < columnCount && board[rowIndex][columnIndex + 1] == 'O') {
+                BoundaryDFS(board, rowIndex, columnIndex + 1);
+            }
+
+            if (columnIndex - 1 >= 0 && board[rowIndex][columnIndex - 1] == 'O') {
+                BoundaryDFS(board, rowIndex, columnIndex - 1);
+            }
+        }
+    }
 }
