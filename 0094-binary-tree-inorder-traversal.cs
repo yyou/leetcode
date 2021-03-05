634770db258ade30506b434c0d88f7fd2d4dc845
Yang You <yy_318@hotmail.com>
2021-03-05 17:24:33 +1100

    Add iteration solution for question 94

--- 0094-binary-tree-inorder-traversal.cs
+++ 0094-binary-tree-inorder-traversal.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace leetcode {
-    public class Solution94 {
+    public class Solution94Recursion {
         public IList<int> InorderTraversal(TreeNode root) {
             IList<int> result = new List<int>();
             InorderTraversal(root, result);
@@ -18,6 +18,26 @@ namespace leetcode {
             InorderTraversal(node.left, result);
             result.Add(node.val);
             InorderTraversal(node.right, result);
+        }
+    }
+
+    public class Solution94Iteration {
+        public IList<int> InorderTraversal(TreeNode root) {
+            IList<int> result = new List<int>();
+            var stack = new Stack<TreeNode>();
+            var cur = root;
+            while (cur != null || stack.Count > 0) {
+                if (cur != null) {
+                    stack.Push(cur);
+                    cur = cur.left;
+                } else {
+                    cur = stack.Pop();
+                    result.Add(cur.val);
+                    cur = cur.right;
+                }
+            }
+
+            return result;
         }
     }
 }
