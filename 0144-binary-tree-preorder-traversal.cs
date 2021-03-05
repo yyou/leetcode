2ab60ec6fc29a2239d21fc503f971505cd367030
Yang You <yy_318@hotmail.com>
2021-03-05 16:27:32 +1100

    Add another solution for question 144 - iteration way

--- 0144-binary-tree-preorder-traversal.cs
+++ 0144-binary-tree-preorder-traversal.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace leetcode {
-    public class Solution144 {
+    public class Solution144Recursion {
         public IList<int> PreorderTraversal(TreeNode root) {
             IList<int> result = new List<int>();
             PreorderTraversal(root, result);
@@ -17,6 +17,24 @@ namespace leetcode {
             result.Add(node.val);
             PreorderTraversal(node.left, result);
             PreorderTraversal(node.right, result);
+        }
+    }
+
+    public class Solution144Iteration {
+        public IList<int> PreorderTraversal(TreeNode root) {
+            IList<int> result = new List<int>();
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count > 0) {
+                var node = stack.Pop();
+                if (node == null) {
+                    continue;
+                }
+                result.Add(node.val);
+                stack.Push(node.right);
+                stack.Push(node.left);
+            }
+            return result;
         }
     }
 }
