d60f9479ff76608f74b9f49757b4f994a1144b63
Yang You <yy_318@hotmail.com>
2021-03-05 22:24:02 +1100

    Add iteration solution of question 226

--- 0226-invert-binary-tree.cs
+++ 0226-invert-binary-tree.cs
@@ -1,7 +1,8 @@
 using System;
+using System.Collections.Generic;
 
 namespace leetcode {
-    public class Solution226 {
+    public class Solution226Recursion {
         public TreeNode InvertTree(TreeNode root) {
             if (root == null) {
                 return null;
@@ -10,6 +11,27 @@ namespace leetcode {
             var leftNode = InvertTree(root.left);
             var rightNode = InvertTree(root.right);
             return new TreeNode(root.val, rightNode, leftNode);
+        }
+    }
+
+    public class Solution226Iteration {
+        public TreeNode InvertTree(TreeNode root) {
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count > 0) {
+                var node = stack.Pop();
+                if (node == null) {
+                    continue;
+                }
+                var tmp = node.left;
+                node.left = node.right;
+                node.right = tmp;
+
+                stack.Push(node.right);
+                stack.Push(node.left);
+            }
+
+            return root;
         }
     }
 }
