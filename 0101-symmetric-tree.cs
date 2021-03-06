ab3aca51afee4d5d889a77c541c72ba1b277b8f6
Yang You <yy_318@hotmail.com>
2021-03-06 22:41:50 +1100

    Add recursion / iteration solution for question 101

--- 0101-symmetric-tree.cs
+++ 0101-symmetric-tree.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace leetcode {
-    public class Solution101 {
+    public class Solution101LevelTraversal {
         public bool IsSymmetric(TreeNode root) {
             var queue = new Queue<TreeNode>();
             queue.Enqueue(root);
@@ -34,6 +34,69 @@ namespace leetcode {
                     i++;
                     j--;
                 }
+            }
+
+            return true;
+        }
+    }
+
+    public class Solution101PostorderTraversal {
+        public bool IsSymmetric(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+
+            return IsSymmetric(root.left, root.right);
+        }
+
+        private bool IsSymmetric(TreeNode leftNode, TreeNode rightNode) {
+            if (leftNode == null && rightNode == null) {
+                return true;
+            }
+
+            if (leftNode == null || rightNode == null) {
+                return false;
+            }
+
+            if (leftNode.val != rightNode.val) {
+                return false;
+            }
+
+            return IsSymmetric(leftNode.left, rightNode.right) &&
+                IsSymmetric(leftNode.right, rightNode.left);
+        }
+    }
+
+    public class Solution101Iteration {
+        public bool IsSymmetric(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root.left);
+            queue.Enqueue(root.right);
+
+            while (queue.Count > 0) {
+                var leftNode = queue.Dequeue();
+                var rightNode = queue.Dequeue();
+
+                if (leftNode == null && rightNode == null) {
+                    continue;
+                }
+
+                if (leftNode == null || rightNode == null) {
+                    return false;
+                }
+
+                if (leftNode.val != rightNode.val) {
+                    return false;
+                }
+
+                queue.Enqueue(leftNode.left);
+                queue.Enqueue(rightNode.right);
+                queue.Enqueue(leftNode.right);
+                queue.Enqueue(rightNode.left);
             }
 
             return true;
