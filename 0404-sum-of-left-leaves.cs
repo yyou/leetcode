c8d9eaede71a73c2237b022b29cded5c3316c3f1
Yang You <yy_318@hotmail.com>
2021-03-10 22:42:46 +1100

    Add iteration solution for question 404

--- 0404-sum-of-left-leaves.cs
+++ 0404-sum-of-left-leaves.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace leetcode {
-    public class Solution404 {
+    public class Solution404Recursion {
         public int SumOfLeftLeaves(TreeNode root) {
             if (root == null) {
                 return 0;
@@ -17,6 +17,37 @@ namespace leetcode {
             sum += SumOfLeftLeaves(root.left);
             sum += SumOfLeftLeaves(root.right);
 
+            return sum;
+        }
+    }
+
+    public class Solution404Iteration {
+        public int SumOfLeftLeaves(TreeNode root) {
+            var st = new Stack<TreeNode>();
+            var sum = 0;
+            if (root != null) {
+                st.Push(root);
+            }
+            while (st.Any()) {
+                var node = st.Pop();
+                if (node != null) {
+                    if (node.right != null) {
+                        st.Push(node.right);
+                    }
+
+                    if (node.left != null) {
+                        st.Push(node.left);
+                    }
+
+                    st.Push(node);
+                    st.Push(null);
+                } else {
+                    var node2 = st.Pop();
+                    if (node2.left != null && node2.left.left == null && node2.left.right == null) {
+                        sum += node2.left.val;
+                    }
+                }
+            }
             return sum;
         }
     }
