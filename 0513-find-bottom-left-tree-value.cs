b73bc1c4b104ec3e914e0b062c354002ad71e64f
Yang You <yy_318@hotmail.com>
2021-03-10 23:23:47 +1100

    Add recursion solution for question 513

--- 0513-find-bottom-left-tree-value.cs
+++ 0513-find-bottom-left-tree-value.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace leetcode {
-    public class Solution513Recursion {
+    public class Solution513LayerTraversal {
         public int FindBottomLeftValue(TreeNode root) {
             var result = 0;
             var queue = new Queue<TreeNode>();
@@ -26,6 +26,38 @@ namespace leetcode {
                 }
             }
             return result;
+        }
+    }
+
+    public class Solution513Recursion {
+        private int maxDepth = Int32.MinValue;
+        private int result = 0;
+
+        public int FindBottomLeftValue(TreeNode root) {
+            FindBottomLeftValue(root, 0);
+            return result;
+        }
+
+        public void FindBottomLeftValue(TreeNode node, int currentDepth) {
+            if (node.left == null && node.right == null) {
+                if (currentDepth > maxDepth) {
+                    maxDepth = currentDepth;
+                    result = node.val;
+                }
+                return;
+            }
+
+            if (node.left != null) {
+                currentDepth++;
+                FindBottomLeftValue(node.left, currentDepth);
+                currentDepth--;
+            }
+
+            if (node.right != null) {
+                currentDepth++;
+                FindBottomLeftValue(node.right, currentDepth);
+                currentDepth--;
+            }
         }
     }
 }
