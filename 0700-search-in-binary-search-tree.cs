f622c47a34a6ac80cc7e70ba3532a30cde03d0bb
Yang You <yy_318@hotmail.com>
2021-03-11 22:16:21 +1100

    Add iteration solution for question 700

--- 0700-search-in-binary-search-tree.cs
+++ 0700-search-in-binary-search-tree.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace leetcode {
-    public class Solution700 {
+    public class Solution700Recursion {
         public TreeNode SearchBST(TreeNode root, int val) {
             if (root == null) {
                 return null;
@@ -18,6 +18,36 @@ namespace leetcode {
             } else {
                 return SearchBST(root.right, val);
             }
+        }
+    }
+
+    public class Solution700Iteration {
+        public TreeNode SearchBST(TreeNode root, int val) {
+            if (root == null) {
+                return null;
+            }
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Any()) {
+                var node = queue.Dequeue();
+                if (node == null) {
+                    return null;
+                }
+
+                if (node.val == val) {
+                    return node;
+                }
+
+                if (node.val > val) {
+                    queue.Enqueue(node.left);
+                }
+
+                if (node.val < val) {
+                    queue.Enqueue(node.right);
+                }
+            }
+            return null;
         }
     }
 }
