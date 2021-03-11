9c3a9ffa53d01b8195d5d4aac1966f7f36b4a6e1
Yang You <yy_318@hotmail.com>
2021-03-11 11:23:31 +1100

    Add iteration solution for question 112

--- 0112-path-sum.cs
+++ 0112-path-sum.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace leetcode {
-    public class Solution112 {
+    public class Solution112Recursion {
         public bool HasPathSum(TreeNode root, int targetSum) {
             if (root == null) {
                 return targetSum == 0;
@@ -20,6 +20,36 @@ namespace leetcode {
             } else {
                 return root.val == targetSum;
             }
+        }
+    }
+
+    public class Solution112Iteration {
+        public bool HasPathSum(TreeNode root, int targetSum) {
+            if (root == null) {
+                return targetSum == 0;
+            }
+
+            var stack = new Stack<Tuple<TreeNode, int>>();
+            stack.Push(new Tuple<TreeNode, int>(root, root.val));
+
+            while (stack.Any()) {
+                var tuple = stack.Pop();
+                if (tuple.Item1.left == null && tuple.Item1.right == null && tuple.Item2 == targetSum) {
+                    return true;
+                }
+
+                var rightChild = tuple.Item1.right;
+                if (rightChild != null) {
+                    stack.Push(new Tuple<TreeNode, int>(rightChild, tuple.Item2 + rightChild.val));
+                }
+
+                var leftChild = tuple.Item1.left;
+                if (leftChild != null) {
+                    stack.Push(new Tuple<TreeNode, int>(leftChild, tuple.Item2 + leftChild.val));
+                }
+            }
+
+            return false;
         }
     }
 }
