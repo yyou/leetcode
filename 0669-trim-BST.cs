f4eda7423fa1c873d27ad2cb5e9136ddaabfa926
Yang You <yy_318@hotmail.com>
2021-03-20 22:29:37 +1100

    Add more solution for question 669

--- 0669-trim-BST.cs
+++ 0669-trim-BST.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace leetcode {
-    public class Solution669BasedOnBSTDeletion {
+    public class Solution669BasedOnBSTDeletion { //complex
         public TreeNode TrimBST(TreeNode root, int low, int high) {
             if (root == null) {
                 return null;
@@ -48,6 +48,66 @@ namespace leetcode {
                     return root;
                 }
             }
+
+            return root;
+        }
+    }
+
+    public class Solution669Another { // better to understand but still contains unnecessary trim operation
+        public TreeNode TrimBST(TreeNode root, int low, int high) {
+            if (root == null) {
+                return null;
+            }
+
+            // These 2 lines contains unnecessary trim operations
+            // We don't need to check every node in BST
+            root.left = TrimBST(root.left, low, high);
+            root.right = TrimBST(root.right, low, high);
+
+            if (root.val >= low && root.val <= high) {
+                return root;
+            }
+
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            if (root.left == null) {
+                return root.right;
+            }
+
+            if (root.right == null) {
+                return root.left;
+            }
+
+            var cur = root.right;
+            while (cur.left != null) {
+                cur = cur.left;
+            }
+
+            cur.left = root.left;
+            return root.right;
+        }
+    }
+
+    public class Solution669Simple { // removed unnecessary trim operations
+        public TreeNode TrimBST(TreeNode root, int low, int high) {
+            if (root == null) {
+                return null;
+            }
+
+            // This step will avoid trimming root.left
+            if (root.val < low) {
+                return TrimBST(root.right, low, high);
+            }
+
+            // This step will avoid trimming root.right
+            if (root.val > high) {
+                return TrimBST(root.left, low, high);
+            }
+
+            root.left = TrimBST(root.left, low, high);
+            root.right = TrimBST(root.right, low, high);
 
             return root;
         }
