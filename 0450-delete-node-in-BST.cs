1d1310cb55a6fad6734b5a99099a1ec15089b1ba
Yang You <yy_318@hotmail.com>
2021-03-20 22:06:00 +1100

    Add another solution for question 450

--- 0450-delete-node-in-BST.cs
+++ 0450-delete-node-in-BST.cs
@@ -3,8 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace leetcode {
-    public class Solution {
+    public class Solution450Recursion {
 
+        public TreeNode DeleteNode(TreeNode root, int key) {
+            if (root == null) {
+                return root;
+            }
+
+            if (root.val == key) {
+                if (root.left == null && root.right == null) {
+                    return null;
+                } else if (root.left == null) {
+                    return root.right;
+                } else if (root.right == null) {
+                    return root.left;
+                } else {
+                    // If the left and right children are not empty, 
+                    // the left subtree head node (left child) of the deleted copy
+                    // is placed on the left child of the leftmost node of the right 
+                    // subtree of the deleted copy, and the right child of the delete
+                    // list is returned as the new root
+                    var cur = root.right;
+                    while (cur.left != null) {
+                        cur = cur.left;
+                    }
+                    cur.left = root.left;
+                    root = root.right;
+                    return root;
+                }
+            }
+
+            if (root.val > key) {
+                root.left = DeleteNode(root.left, key);
+            } else if (root.val < key) {
+                root.right = DeleteNode(root.right, key);
+            }
+
+            return root;
+        }
+    }
+
+    public class Solution450AnotherRecursion {
         public TreeNode DeleteNode(TreeNode root, int key) {
             if (root == null) {
                 return root;
@@ -22,12 +61,15 @@ namespace leetcode {
                 } else if (root.right == null) {
                     return root.left;
                 } else {
-                    var cur = root.right;
-                    while (cur.left != null) {
-                        cur = cur.left;
+                    // Find the most right node of current node's left child
+                    // and replace the current node with that node
+                    var node = root.left;
+                    while (node.right != null) {
+                        node = node.right;
                     }
-                    cur.left = root.left;
-                    root = root.right;
+                    var val = node.val;
+                    root.left = DeleteNode(root.left, val);
+                    root.val = val;
                     return root;
                 }
             }
