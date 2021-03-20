0fca027b48b6542905c0247bb1ddf209ce56c2a4
Yang You <yy_318@hotmail.com>
2021-03-20 20:17:23 +1100

    Add iteration solution for question 701

--- 0701-insert-value-into-BST.cs
+++ 0701-insert-value-into-BST.cs
@@ -20,4 +20,32 @@ namespace leetcode {
             return node;
         }
     }
+
+    public class Solution701Iteration {
+        public TreeNode InsertIntoBST(TreeNode root, int val) {
+            if (root == null) {
+                return new TreeNode(val);
+            }
+
+            var node = root;
+            while (node != null) {
+                if (node.val > val) {
+                    if (node.left == null) {
+                        node.left = new TreeNode(val);
+                        return root;
+                    } else {
+                        node = node.left;
+                    }
+                } else {
+                    if (node.right == null) {
+                        node.right = new TreeNode(val);
+                        return root;
+                    } else {
+                        node = node.right;
+                    }
+                }
+            }
+            return root;
+        }
+    }
 }
