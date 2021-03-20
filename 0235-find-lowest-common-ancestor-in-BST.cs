5d1976e7ac0dfe735a42a37d61b127fba7f6b402
Yang You <yy_318@hotmail.com>
2021-03-20 19:35:37 +1100

    Add iteration solution for question 235

--- 0235-find-lowest-common-ancestor-in-BST.cs
+++ 0235-find-lowest-common-ancestor-in-BST.cs
@@ -26,4 +26,19 @@ namespace leetcode {
             return root;
         }
     }
+
+    public class Solution235Iteration {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            while (root != null) {
+                if (root.val > p.val && root.val > q.val) {
+                    root = root.left;
+                } else if (root.val < p.val && root.val < q.val) {
+                    root = root.right;
+                } else {
+                    return root;
+                }
+            }
+            return null;
+        }
+    }
 }
