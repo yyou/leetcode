fa55167f0b4818b7b8cf779ae44e23b80dfa392b
Yang You <yy_318@hotmail.com>
2021-03-22 00:37:21 +1100

    Refactor solution for question 216

--- 0216-combination-sum-III.cs
+++ 0216-combination-sum-III.cs
@@ -8,27 +8,33 @@ namespace leetcode {
         private IList<IList<int>> _result = new List<IList<int>>();
 
         public IList<IList<int>> CombinationSum3(int k, int n) {
-            BackTracking(k, n, 1);
+            BackTracking(k, n, 0, 1);
             return _result;
         }
 
-        private void BackTracking(int k, int n, int startIndex) {
-            if (_path.Count == k && _path.Sum() == n) {
-                var clonedPath = new List<int>();
-                foreach (var p in _path) {
-                    clonedPath.Add(p);
-                }
-                _result.Add(clonedPath);
+        private void BackTracking(int k, int targetSum, int currentSum, int startIndex) {
+            if (currentSum > targetSum) {
                 return;
             }
 
             if (_path.Count == k) {
+                if (currentSum == targetSum) {
+                    var clonedPath = new List<int>();
+                    foreach (var p in _path) {
+                        clonedPath.Add(p);
+                    }
+                    _result.Add(clonedPath);
+                }
                 return;
             }
 
             for (var i = startIndex; i <= 9; ++i) {
                 _path.Add(i);
-                BackTracking(k, n, i + 1);
+                currentSum += i;
+
+                BackTracking(k, targetSum, currentSum, i + 1);
+
+                currentSum -= i;
                 _path.RemoveAt(_path.Count - 1);
             }
         }
