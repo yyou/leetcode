0e9f142136eeef42a843f0c220cd720fed58ff34
yyou <yy_318@hotmail.com>
2022-07-13 23:04:12 +1000

    Add solution for question 55 - jump game

---
+++ 0055-jump-game.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace leetcode {
+    public class Solution0055 {
+        public bool CanJump(int[] nums) {
+            var cover = 0;
+            for (var i = 0; i <= cover; i++) {
+                cover = Math.Max(nums[i] + i, cover);
+                if (cover >= nums.Length - 1) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
