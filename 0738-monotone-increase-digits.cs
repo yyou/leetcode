c57fcc9ecec75973e87ac2490c8923bcb15871f7
yyou <yy_318@hotmail.com>
2022-07-25 00:16:57 +1000

    Add solution for question 738 - monotone increasing digits

---
+++ 0738-monotone-increase-digits.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace leetcode {
+    public class Solution738 {
+        public int MonotoneIncreasingDigits(int n) {
+            var chars = n.ToString().ToCharArray();
+
+            var flag = chars.Length;
+            for (var i = chars.Length - 1; i > 0; i--) {
+                if (chars[i - 1] > chars[i]) {
+                    flag = i;
+                    chars[i - 1]--;
+                }
+            }
+
+            for (var i = flag; i < chars.Length; ++i) {
+                chars[i] = '9';
+            }
+
+            var s = String.Join("", chars);
+            return Int32.Parse(s);
+        }
+    }
+}
