164372dc0943b6d04e326867d4b4d37a80757857
yyou <yy_318@hotmail.com>
2022-07-26 22:17:36 +1000

    Make variable name more meaningful for solution 509

--- 0509-fabonacci-number.cs
+++ 0509-fabonacci-number.cs
@@ -1,6 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 namespace leetcode {
     public class Solution509 {
@@ -17,9 +17,9 @@ namespace leetcode {
             var n1 = 1;
             var idx = 3;
             while (idx <= n) {
-                var tmp = n1 + n0;
+                var sum = n1 + n0;
                 n0 = n1;
-                n1 = tmp;
+                n1 = sum;
                 idx++;
             }
             return n1;
