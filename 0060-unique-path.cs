20cea6dd7149c93695fa74877a2021dfbfbc6b96
yyou <yy_318@hotmail.com>
2022-08-27 23:35:31 +1000

    Add solution for question 60 - unique path

---
+++ 0060-unique-path.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace leetcode {
+    public class Solution60 {
+        public int UniquePaths(int m, int n) {
+            int[,] a = new int[m, n];
+
+            for (var i = 0; i < n; ++i) {
+                a[0, i] = 1;
+            }
+
+            for (var i = 0; i < m; ++i) {
+                a[i, 0] = 1;
+            }
+
+            for (var i = 1; i < m; ++i) {
+                for (var j = 1; j < n; ++j) {
+                    a[i, j] = a[i - 1, j] + a[i, j - 1];
+                }
+            }
+
+            return a[m - 1, n - 1];
+        }
+    }
+}
+
--- 0142-linked-list-cycle-II.cs
+++ 0142-linked-list-cycle-II.cs
@@ -1,9 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 namespace leetcode {
-    public class Solution {
+    public class Solution142 {
         public ListNode DetectCycle(ListNode head) {
             var slow = head;
             var fast = head;
