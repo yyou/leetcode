0133dae21fa415715b942688755c038892ea1d72
yyou <yy_318@hotmail.com>
2022-08-28 21:50:45 +1000

    Add solution for question 343 - integer break

---
+++ 0343-integer-break.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace leetcode {
+    public class Solution343 {
+        public int IntegerBreak(int n) {
+            var dp = new int[n + 1];
+            dp[2] = 1;
+
+            for (var i = 3; i <= n; ++i) {
+                for (var j = 1; j < i - 1; j++) {
+                    dp[i] = Math.Max(dp[i], Math.Max(j * dp[i - j], j * (i - j)));
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
