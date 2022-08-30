e96431b8e52ceb4a05a8511b14a42ed05c0e8fc3
yyou <yy_318@hotmail.com>
2022-08-30 19:56:21 +1000

    Add solution for question 96 - unique binary trees

---
+++ 0096-unique-binary-trees.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace leetcode {
+    public class Solution96 {
+        public int NumTrees(int n) {
+            var dp = new int[n + 1];
+            dp[0] = 1;
+
+            for (var i = 1; i <= n; ++i) {
+                for (var j = 1; j <= i; ++j) {
+                    dp[i] += dp[j - 1] * dp[i - j];
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
