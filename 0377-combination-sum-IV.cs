e92f5e748a1b6fd97ad0d507af6dca341d005a23
yyou <yy_318@hotmail.com>
2022-09-15 22:37:19 +1000

    Add solution for question 377 - combination sum IV

---
+++ 0377-combination-sum-IV.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace leetcode {
+    public class Solution377 {
+        public int CombinationSum4(int[] nums, int target) {
+            var dp = new int[target + 1];
+            dp[0] = 1;
+            for (var j = 1; j <= target; ++j) {
+                for (var i = 0; i < nums.Length; ++i) {
+                    if (j >= nums[i]) {
+                        dp[j] += dp[j - nums[i]];
+                    }
+                }
+            }
+            return dp[target];
+        }
+    }
+}
