ba7d0c88317109042b9486c24a5a18ef82006f54
yyou <yy_318@hotmail.com>
2022-09-15 22:26:58 +1000

    Add solution for question 518 - coin change II

---
+++ 0518-coin-change-II.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace leetcode {
+    public class Solution518 {
+        public int Change(int amount, int[] coins) {
+            var dp = new int[amount + 1];
+            dp[0] = 1;
+            for (var i = 0; i < coins.Length; ++i) {
+                for (var j = coins[i]; j <= amount; ++j) {
+                    dp[j] += dp[j - coins[i]];
+                }
+            }
+
+            return dp[amount];
+        }
+    }
+}
