8bc3b2b81c06db796624f8c37c5873729312125e
yyou <yy_318@hotmail.com>
2022-09-23 22:00:05 +1000

    Refactor the solution for question 121

--- 0121-best-time-to-buy-sell-stock.cs
+++ 0121-best-time-to-buy-sell-stock.cs
@@ -1,15 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System;
 
 namespace leetcode {
     public class Solution121 {
         public int MaxProfit(int[] prices) {
-            var dp_i_0 = 0;
-            var dp_i_1 = Int32.MinValue;
-            for (var i = 0; i < prices.Length; ++i) {
-                dp_i_0 = Math.Max(dp_i_0, dp_i_1 + prices[i]);
-                dp_i_1 = Math.Max(dp_i_1, -prices[i]);
+            var size = prices.Length;
+            if (size == 0) {
+                return 0;
             }
-            return dp_i_0;
+
+            //[i,0] represents the max value from holding the share on the ith day
+            //[i,1] represents the max value from not holding the share on th e ith day
+            var dp = new int[size, 2];
+
+            dp[0, 0] = -prices[0];
+            dp[0, 1] = 0;
+
+            for (var i = 1; i < prices.Length; ++i) {
+                dp[i, 0] = Math.Max(dp[i - 1, 0], -prices[i]);
+                dp[i, 1] = Math.Max(dp[i - 1, 1], dp[i - 1, 0] + prices[i]);
+            }
+
+            return Math.Max(dp[size - 1, 0], dp[size - 1, 1]);
         }
+
+
+        public int MaxProfit2(int[] prices) {
+            var size = prices.Length;
+            if (size == 0) {
+                return 0;
+            }
+
+            //[0] represents the max value from holding the share on the current day
+            //[1] represents the max value from not holding the share on the current day
+            var dp = new int[2];
+
+            dp[0] = -prices[0];
+            dp[1] = 0;
+
+            for (var i = 1; i < prices.Length; ++i) {
+                dp[0] = Math.Max(dp[0], -prices[i]);
+                dp[1] = Math.Max(dp[1], dp[0] + prices[i]);
+            }
+
+            return Math.Max(dp[0], dp[1]);
+        }
+
     }
 }
