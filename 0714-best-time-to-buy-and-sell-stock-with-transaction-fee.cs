4ac245c2da731448205761faef7c6cfef50b8026
yyou <yy_318@hotmail.com>
2022-09-26 21:32:45 +1000

    Add solution to question 714 - best time to buy and sell stock with transaction fee

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace leetcode {
+    public class Solution714 {
+        public int MaxProfit(int[] prices, int fee) {
+            var size = prices.Length;
+            if (size == 0) {
+                return 0;
+            }
+
+            // 2 statuses:
+            // status 0: holding the share
+            // status 1: not holding the share
+            var dp = new int[size, 2];
+            dp[0, 0] = -prices[0];
+            dp[0, 1] = 0;
+
+            for (var i = 1; i < size; ++i) {
+                dp[i, 0] = Math.Max(dp[i - 1, 0], dp[i - 1, 1] - prices[i]);
+                dp[i, 1] = Math.Max(dp[i - 1, 1], dp[i - 1, 0] + prices[i] - fee);
+            }
+
+            return dp[size - 1, 1];
+        }
+    }
+}
