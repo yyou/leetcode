c943281cb633be0eb9fbfc685e1690e09d11f4f6
yyou <yy_318@hotmail.com>
2022-10-01 22:22:49 +1000

    Add solution for question 26 - remove duplicate from sorted array

---
+++ 0026-remove-duplicate-from-sorted-array.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace leetcode {
+    public class Solution26 {
+        public int RemoveDuplicates(int[] nums) {
+            var slowIndex = 0;
+            for (var fastIndex = 0; fastIndex < nums.Length - 1; ++fastIndex) {
+                if (nums[fastIndex] != nums[fastIndex + 1]) {
+                    nums[slowIndex] = nums[fastIndex];
+                    slowIndex++;
+                }
+            }
+
+            nums[slowIndex] = nums[nums.Length - 1];
+            slowIndex++;
+            return slowIndex;
+        }
+    }
+}
