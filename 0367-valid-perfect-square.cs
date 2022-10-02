2dce0d0d53bccfcbd9bc1b1e79f3d59d7a6fad4b
yyou <yy_318@hotmail.com>
2022-10-02 22:37:46 +1100

    Add solution for question 367 - valid perfect square

---
+++ 0367-valid-perfect-square.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace leetcode {
+    public class Solution367 {
+        public bool IsPerfectSquare(int num) {
+            if (num == 1) {
+                return true;
+            }
+
+            long left = 0;
+            long right = num / 2;
+            while (left <= right) {
+                long middle = left + (right - left) / 2;
+                long product = middle * middle;
+                if (product == num) {
+                    return true;
+                } else if (product > num) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
