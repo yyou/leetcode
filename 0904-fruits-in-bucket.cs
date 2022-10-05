a2e0b3b01014d487145ed4b87967889d2d311b74
yyou <yy_318@hotmail.com>
2022-10-05 22:50:32 +1100

    Add solution for question 904  - fruits in buckets

---
+++ 0904-fruits-in-bucket.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Linq;
+
+namespace leetcode {
+    public class Solution904 {
+
+        public int TotalFruit(int[] fruits) {
+            var result = 0;
+            var dict = new ItemCountDictionary<int>();
+            var left = 0;
+            for (var right = 0; right < fruits.Length; ++right) {
+                dict[fruits[right]]++;
+
+                while (dict.Count() > 2) {
+                    dict[fruits[left]]--;
+                    if (dict[fruits[left]] == 0) {
+                        dict.Remove(fruits[left]);
+                    }
+                    left++;
+                }
+
+                result = Math.Max(result, right - left + 1);
+            }
+
+            return result;
+        }
+    }
+
+}
