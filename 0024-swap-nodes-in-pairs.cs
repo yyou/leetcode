9da497f638ee978c33e2a680ed577f8920eaad17
yyou <yy_318@hotmail.com>
2022-10-12 22:16:11 +1100

    Add solution to question 24 - swap nodes in pairs

---
+++ 0024-swap-nodes-in-pairs.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace leetcode {
+    public class Solution24Recusive {
+        public ListNode SwapPairs(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+
+            if (head.next == null) {
+                return head;
+            }
+
+            var third = head.next.next;
+            var next = head.next;
+            next.next = head;
+            head.next = SwapPairs(third);
+
+            return next;
+        }
+    }
+}
