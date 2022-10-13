7bbf1efe8cbf3694f8727559de8ebe6ab35fc9f7
yyou <yy_318@hotmail.com>
2022-10-13 22:32:39 +1100

    Refactor solution for question 242

--- 0242-valid-anagram.cs
+++ 0242-valid-anagram.cs
@@ -1,23 +1,16 @@
-using System.Collections.Generic;
-using System.Linq;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 namespace leetcode {
     public class Solution242 {
         public bool IsAnagram(string s, string t) {
             var a = new int[26];
-            for (var i = 0; i < 26; ++i) {
-                a[i] = 0;
-            }
-
             for (var i = 0; i < s.Length; ++i) {
                 a[s[i] - 'a']++;
             }
 
             var b = new int[26];
-            for (var i = 0; i < 26; ++i) {
-                b[i] = 0;
-            }
-
             for (var i = 0; i < t.Length; ++i) {
                 b[t[i] - 'a']++;
             }
