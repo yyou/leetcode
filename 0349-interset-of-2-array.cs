4cfef49c0b12ec4e0f4b838dc4fde5d310ad15b3
yyou <yy_318@hotmail.com>
2022-10-14 22:42:15 +1100

    Add new solution to question 349 - using array for hashtable

--- 0349-interset-of-2-array.cs
+++ 0349-interset-of-2-array.cs
@@ -1,13 +1,30 @@
-using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Collections.Generic;
-using System.Linq;
 
 namespace leetcode {
     public class Solution349 {
         public int[] Intersection(int[] nums1, int[] nums2) {
-            var list1 = new List<int>(nums1);
-            var list2 = new List<int>(nums2);
-            return list1.Intersect(list2).ToArray();
+            var a1 = new int[1001];
+            foreach (var i in nums1) {
+                a1[i]++;
+            }
+
+            var a2 = new int[1001];
+            foreach (var i in nums2) {
+                a2[i]++;
+            }
+
+            var intersect = new List<int>();
+            for (var i = 0; i <= 1000; i++) {
+                if (a1[i] > 0 && a2[i] > 0) {
+                    intersect.Add(i);
+                }
+            }
+
+            return intersect.ToArray();
         }
     }
 }
