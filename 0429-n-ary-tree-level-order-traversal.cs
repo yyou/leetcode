d4aab441977d0236b188007928b3ea6658304f04
yyou <yy_318@hotmail.com>
2022-11-04 22:40:53 +1100

    Add solution for question 429 - N-ary tree level order traversal

---
+++ 0429-n-ary-tree-level-order-traversal.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode {
+    public class Solution429 {
+        public IList<IList<int>> LevelOrder(NaryTreeNode root) {
+            var result = new List<IList<int>>();
+            var queue = new Queue<NaryTreeNode>();
+            if (root != null) {
+                queue.Enqueue(root);
+            }
+
+            while (queue.Any()) {
+                var size = queue.Count();
+                var values = new List<int>();
+                for (var i = 0; i < size; ++i) {
+                    var node = queue.Dequeue();
+                    values.Add(node.val);
+                    if (node.children != null) {
+                        for (var j = 0; j < node.children.Count(); ++j) {
+                            queue.Enqueue(node.children[j]);
+                        }
+                    }
+                }
+                result.Add(values);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ NaryTreeNode.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace leetcode {
+    public class NaryTreeNode {
+        public int val;
+        public IList<NaryTreeNode> children;
+
+        public NaryTreeNode() { }
+
+        public NaryTreeNode(int _val) {
+            val = _val;
+        }
+
+        public NaryTreeNode(int _val, IList<NaryTreeNode> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+}
