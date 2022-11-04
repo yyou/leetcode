61bebe6bfa18dd113ee5928effe14b3f5fea05db
yyou <yy_318@hotmail.com>
2022-11-04 22:46:52 +1100

    Add solution for questino 515 - find largest value in tree row

---
+++ 0515-find-largest-value-in-tree-row.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+
+namespace leetcode {
+    public class Solution515 {
+        public IList<int> LargestValues(TreeNode root) {
+            var queue = new Queue<TreeNode>();
+            if (root != null) {
+                queue.Enqueue(root);
+            }
+
+            var result = new List<int>();
+            while (queue.Count > 0) {
+                var size = queue.Count;
+                var max = Int32.MinValue;
+                for (var i = 0; i < size; ++i) {
+                    var node = queue.Dequeue();
+                    max = Math.Max(max, node.val);
+                    if (node.left != null) {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                result.Add(max);
+            }
+
+            return result;
+        }
+    }
+}
