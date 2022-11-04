6ebe96d0a75cb1eebb977a5fca4f0444e8945bb8
yyou <yy_318@hotmail.com>
2022-11-04 22:26:03 +1100

    Refactor the solution for question 637

--- 0637-average-level-of-binary-tree.cs
+++ 0637-average-level-of-binary-tree.cs
@@ -1,5 +1,9 @@
-using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Collections.Generic;
+using System.Linq;
 
 namespace leetcode {
     public class Solution637 {
@@ -10,9 +14,8 @@ namespace leetcode {
             }
 
             var result = new List<double>();
-            while (queue.Count > 0) {
+            while (queue.Any()) {
                 var size = queue.Count;
-                var values = new List<int>();
                 var sum = 0.0d;
                 for (var i = 0; i < size; ++i) {
                     var node = queue.Dequeue();
