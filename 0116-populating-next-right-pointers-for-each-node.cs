7280411f38c7c6cee08eca246158f99215a468b4
yyou <yy_318@hotmail.com>
2022-11-04 23:19:29 +1100

    Add iteration solution to question 116

--- 0116-populating-next-right-pointers-for-each-node.cs
+++ 0116-populating-next-right-pointers-for-each-node.cs
@@ -1,7 +1,11 @@
-using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+using System.Collections.Generic;
+using System.Linq;
 
 namespace leetcode {
-    public class Solution116 {
+    public class Solution116Recursion {
         public Node Connect(Node root) {
             if (root == null) {
                 return null;
@@ -21,6 +25,34 @@ namespace leetcode {
             Connect(node2.left, node2.right);
             Connect(node1.right, node2.left);
             return;
+        }
+    }
+
+    public class Solution116Iteration {
+        public Node Connect(Node root) {
+            var queue = new Queue<Node>();
+            if (root != null) {
+                queue.Enqueue(root);
+            }
+
+            while (queue.Any()) {
+                var size = queue.Count;
+                for (var i = 0; i < size; ++i) {
+                    var node = queue.Dequeue();
+                    if (i < size - 1) {
+                        node.next = queue.Peek();
+                    }
+
+                    if (node.left != null) {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.Enqueue(node.right);
+                    }
+                }
+            }
+
+            return root;
         }
     }
 }
