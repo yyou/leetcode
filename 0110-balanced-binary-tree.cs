58fe8fe9b6a8e96acb7881ba51b6cfcd013a1adb
yyou <yy_318@hotmail.com>
2022-11-07 23:00:15 +1100

    Add solution to question 110 - balanced binary tree

---
+++ 0110-balanced-binary-tree.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace leetcode {
+    public class Solution110Recursion {
+        public bool IsBalanced(TreeNode root) {
+            return GetHeight(root) != -1;
+        }
+
+        private int GetHeight(TreeNode node) {
+            if (node == null) {
+                return 0;
+            }
+
+            var leftHeight = GetHeight(node.left);
+            if (leftHeight == -1) {
+                return -1;
+            }
+
+            var rightHeight = GetHeight(node.right);
+            if (rightHeight == -1) {
+                return -1;
+            }
+
+            if (Math.Abs(leftHeight - rightHeight) > 1) {
+                return -1;
+            }
+
+            return Math.Max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
