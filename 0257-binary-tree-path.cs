f3f9e929483a4de15ca0733711fed3512490f53b
yyou <yy_318@hotmail.com>
2022-11-09 21:53:30 +1100

    Refactor the solution for question 257

--- 0257-binary-tree-path.cs
+++ 0257-binary-tree-path.cs
@@ -1,13 +1,15 @@
-using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Collections.Generic;
-using System.Linq;
 
 namespace leetcode {
-    public class Solution0257 {
-        private List<String> _result;
+    public class Solution0257Recursion {
+        private List<string> _result;
 
         public IList<string> BinaryTreePaths(TreeNode root) {
-            _result = new List<String>();
+            _result = new List<string>();
             var nums = new List<int>();
 
             if (root != null) {
@@ -22,7 +24,7 @@ namespace leetcode {
             nums.Add(node.val);
 
             if (node.left == null && node.right == null) {
-                _result.Add(String.Join("->", nums.ToArray()));
+                _result.Add(string.Join("->", nums.ToArray()));
             }
 
             if (node.left != null) {
