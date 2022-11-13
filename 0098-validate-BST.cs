4e0b7e7055ca8745faebd3b2ba6dc5726d0ca28c
yyou <yy_318@hotmail.com>
2022-11-13 22:39:00 +1100

    Refactor the solutions to question 98

--- 0098-validate-BST.cs
+++ 0098-validate-BST.cs
@@ -1,6 +1,8 @@
-using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Collections.Generic;
-using System.Linq;
 
 namespace leetcode {
     public class Solution98 {
@@ -69,7 +71,7 @@ namespace leetcode {
         }
     }
 
-    public class Solution98Recursion {
+    public class Solution98RecursivelyByPreNode {
         private TreeNode pre;
         public bool IsValidBST(TreeNode root) {
             if (root == null) {
@@ -94,7 +96,7 @@ namespace leetcode {
         }
     }
 
-    public class Solution98Simple {
+    public class Solution98ByMinMaxNodes {
         public bool IsValidBST(TreeNode root) {
             return IsValidBST(root, null, null);
         }
@@ -113,6 +115,59 @@ namespace leetcode {
             }
 
             return IsValidBST(root.left, min, root) && IsValidBST(root.right, root, max);
+        }
+    }
+
+    public class Solution98ByMinMaxValues {
+        public bool IsValidBST(TreeNode root) {
+            return IsValidBST(root, long.MinValue, long.MaxValue);
+        }
+
+        // as the node value is in range between Int32.MinValue and Int32.MaxValue,
+        // need to use long as the type of 'min' and 'max'.
+        private bool IsValidBST(TreeNode root, long min, long max) {
+            if (root == null) {
+                return true;
+            }
+
+            if (root.val <= min || root.val >= max) {
+                return false;
+            }
+
+            return IsValidBST(root.left, min, root.val) &&
+                IsValidBST(root.right, root.val, max);
+        }
+    }
+
+    // BST values should be in order. So we convert it to a list and then compare the numbers.
+    public class Solution98ByArray {
+        public bool IsValidBST(TreeNode root) {
+            var nums = new List<int>();
+            GetNumbers(root, nums);
+
+            for (var i = 0; i < nums.Count - 1; ++i) {
+                if (nums[i] >= nums[i + 1]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void GetNumbers(TreeNode root, List<int> numbers) {
+            if (root == null) {
+                return;
+            }
+
+            if (root.left != null) {
+                GetNumbers(root.left, numbers);
+            }
+
+            numbers.Add(root.val);
+
+            if (root.right != null) {
+                GetNumbers(root.right, numbers);
+            }
         }
     }
 }
