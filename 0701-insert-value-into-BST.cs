b31f172161b73ef230ebcaae0e7e4596c35ae625
yyou <yy_318@hotmail.com>
2022-11-15 23:34:31 +1100

    Add another solution for question 701

--- 0701-insert-value-into-BST.cs
+++ 0701-insert-value-into-BST.cs
@@ -1,23 +1,53 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 namespace leetcode {
     public class Solution701Recursion {
         public TreeNode InsertIntoBST(TreeNode root, int val) {
-            return InsertValue(root, val);
-        }
-
-        private TreeNode InsertValue(TreeNode node, int val) {
-            if (node == null) {
+            if (root == null) {
                 return new TreeNode(val);
             }
-            if (node.val > val) {
-                node.left = InsertValue(node.left, val);
+            if (root.val > val) {
+                root.left = InsertIntoBST(root.left, val);
             } else {
-                node.right = InsertValue(node.right, val);
+                root.right = InsertIntoBST(root.right, val);
             }
-            return node;
+            return root;
+        }
+    }
+
+    public class Solution701Recursion2 {
+        private TreeNode _parent;
+
+        public TreeNode InsertIntoBST(TreeNode root, int val) {
+            if (root == null) {
+                return new TreeNode(val);
+            }
+
+            Traversal(root, val);
+
+            return root;
+        }
+
+        private void Traversal(TreeNode node, int val) {
+            if (node == null) {
+                if (_parent.val > val) {
+                    _parent.left = new TreeNode(val);
+                } else {
+                    _parent.right = new TreeNode(val);
+                }
+
+                return;
+            }
+
+            _parent = node;
+
+            if (node.val > val) {
+                Traversal(node.left, val);
+            } else {
+                Traversal(node.right, val);
+            }
         }
     }
 
